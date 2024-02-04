2a75fa4670998e24631ddd98fff95d25e7683dea
amitabhiam <amitabhs132@gmail.com>
2024-02-04 16:23:51 +0530

    ui updates

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,39 +2,62 @@
 
 const Login = () => {
   return (
-    <div className="flex justify-center">
-        <div className="w-[400px] h-[800px]">
-            <img src="https://advancelam.com/wp-content/uploads/2022/08/102.jpg" alt="" />
+    <div className="flex h-[100vh] w-[100%]">
+        <div className="w-[60%] h-[auto] ml-0">
+            <img src="/images/Login.jpeg" alt="" className="w-[100%]"/>
         </div>
 
-        <div>
-            <label>Emp Id</label>
+        <div className="flex flex-col items-center w-[40%]  mt-[128px] ">
+            <div className="text-[32px] text-[#868484] font-bold ">
+                Logo
+            </div>
+            <div className="text-[36px] font-extrabold">Welcome Back!</div>
+            <div className="flex mx-[114px] text-[20px] font-normal text-center mt-3">To begin this journey, tell us what type of
+account you’d be assigned.</div>
+<div className="mt-[77px]">
+<div className="text-[20px] text-[#000000] font-normal text-start">
+Emp Id
+</div>
+    <div className="">
+        <input
+      type="text"
+      className="border-2 border-black rounded-md p-2 w-[324px]"
+      placeholder="Enter Id"
+    />
+            </div>
+            <div className="text-[20px] text-[#000000] font-normal text-start mt-[21px]">
+Password
+</div>
+    <div className="">
+        <input
+      type="password"
+      className="border-2 border-black rounded-md p-2 w-[324px]"
+      placeholder="Enter Password"
+    />
+            </div>
+<div className="flex mt-[52px] items-center justify-center w-[324px] gap-3">
+            <div className=" flex bg-[#F3EEF5] border-2   h-[44px] rounded-md items-center ">
+                <div className="font-normal text-[20px] tracking-[8px] px-[38px] ">673490</div>
+            </div>
+            <div>Captcha</div>
+            </div>
             <div className="">
-                <input
-                    type="email"
-                    placeholder="Enter your email"
-                    id="email"
-                />
+        <input
+      type="password"
+      className="border-2 border-black rounded-md p-2 w-[324px] mt-[30px]"
+      placeholder="Enter Captcha"
+    />
+            </div>
+            <div className="flex mt-[25px]">
+            <div><input type="checkbox" className="mr-[10px]" /></div>
+              <div className="text-[15px] font-semibold">I agree to terms & condition</div>
+            </div>
             </div>
 
-            <label>Password</label>
-            <div className="">
-                <input
-                    type="password"
-                    placeholder="Enter your password"
-                    id="password"
-                />
-            </div>
+           
 
-            <div className="">
-                <input
-                    type=""
-                    placeholder="Enter Captcha"
-                    id="password"
-                />
-            </div>
+    
         </div>
-        
     </div>
     
   )
