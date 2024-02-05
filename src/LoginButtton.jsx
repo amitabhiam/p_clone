59a73aac285b79e3a4760b2453ae4ddc622127b5
amitabhiam <amitabhs132@gmail.com>
2024-02-05 22:51:20 +0530

    Login button added

---
+++ src/LoginButtton.jsx
@@ -0,0 +1,11 @@
+
+
+const LoginButtton = () => {
+    return (
+    <div>
+        <button className="bg-[#070562] text-[#F6F6F6] w-[324px] h-[44px]">Login</button>
+    </div>
+    )
+}
+
+export default LoginButtton;
