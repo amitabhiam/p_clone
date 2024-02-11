48ef0de0b8e48412cddaa5c953bb72b8e2d4ba31
amitabhiam <amitabhs132@gmail.com>
2024-02-11 10:29:34 +0530

    Login updates

--- src/LoginButtton.jsx
+++ src/LoginButtton.jsx
@@ -3,7 +3,8 @@
 const LoginButtton = () => {
     return (
     <div>
-        <button className="bg-[#070562] text-[#F6F6F6] w-[324px] h-[44px]">Login</button>
+        <button 
+            className="bg-[#070562] text-[#F6F6F6] w-[324px] h-[44px] rounded-md">Login</button>
     </div>
     )
 }
