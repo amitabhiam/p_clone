1c800f5591a672fdff04c24c5c1a3c8f4ac3279a
amitabhiam <amitabhs132@gmail.com>
2024-02-12 23:10:59 +0530

    Forgot password added

---
+++ src/Dashboard/ForgotPassword.jsx
@@ -0,0 +1,13 @@
+
+
+const ForgotPassword = () => {
+    return (
+    <div>
+        <div>
+            <img src="/images/Login.jpeg" alt="banner" />
+        </div>
+    </div>
+    )
+}
+
+export default ForgotPassword;
