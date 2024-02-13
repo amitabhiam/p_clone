c18703c628b146f01cbc69ff2057ff7ad4adfcfc
amitabhiam <amitabhs132@gmail.com>
2024-02-13 23:12:31 +0530

    modification

--- src/Dashboard/ForgotPassword.jsx
+++ src/Dashboard/ForgotPassword.jsx
@@ -3,6 +3,15 @@ const ForgotPassword = () => {
     return (
     <div>
         <div>
+            <div>
+                Emp Id
+            </div>
+            <input
+                type="text"
+                placeholder="Emp ID"
+            />
+        </div>
+        <div>
             <img src="/images/Login.jpeg" alt="banner" />
         </div>
     </div>
