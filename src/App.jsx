23a9d5880b7c22a89f1f4c5ce3128a2bf18fd7fd
amitabhiam <amitabhs132@gmail.com>
2024-02-14 23:03:28 +0530

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import Login from "./Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" 
+          element={<Login />}
+        />
+      </Routes>
+    </BrowserRouter>
+    </>
+    
   )
 }
