ec14ef4cba906f8a91c551f31872664cd19fd981
amitabhiam <amitabhs132@gmail.com>
2024-02-21 23:55:34 +0530

    changes

---
+++ src/LoginHeader.jsx
@@ -0,0 +1,16 @@
+
+
+const LoginHeader = () => {
+
+    return (
+        <div className="fixed w-[1367px] h-[34px] flex bg-indigo-800">
+            <div>
+                <div className="text-[12px] left-[18px]">
+                    support@pfrda.org.in
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginHeader;
