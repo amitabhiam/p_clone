594a7776080d51bb764b7d8c4dcd445815f6d713
Abhishek Bhardwaj <eduandabhi@gmail.com>
2024-02-21 22:49:48 +0530

    sticky header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="w-full h-[40px] bg-[#3E4095] sticky top-0">
+      <div className="text-white">support@pfrda.org.in</div>
+    </div>
+  );
+}
+
+export default Header;
